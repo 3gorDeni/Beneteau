25c9377e95decc2fe7b4579384acdae5cb10f88a
EgorDeni <84186520+3gorDeni@users.noreply.github.com>
2021-05-20 14:07:48 +0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp13
+{
+	class Boat
+	{
+		public Boat(string v1, string v2, string v3, string v4)
+		{
+			V1 = v1;
+			V2 = v2;
+			V3 = v3;
+			V4 = v4;
+		}
+
+		public string V1 { get; }
+		public string V2 { get; }
+		public string V3 { get; }
+		public string V4 { get; }
+		internal void name (string v)
+		{
+			Console.WriteLine( V1 + ":" + v);
+		}
+		internal void color(string v)
+		{
+			Console.WriteLine(V1 +  ":" + v);
+		}
+		internal void model(string v)
+		{
+			Console.WriteLine(V1 + ":" + v);
+		}
+		internal void speed (string v)
+		{
+			Console.WriteLine(V1 + ":" + v);
+		}
+		internal void move (Boat v)
+		{
+			Console.WriteLine("Beneteau move");
+		}
+		internal void stop (Boat v)
+		{
+			Console.WriteLine("Beneteau stope");
+		}
+	}   
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+	
+		class Program
+		{
+		    static void Main(string[] args)
+			{
+
+				Boat Beneteau = new Boat("Beneteau", "White", "ER-256", "100 km/h");
+			    Beneteau.name("Benetaeu");
+			    Beneteau.color("White");
+			    Beneteau.model("ER-256");
+			    Beneteau.speed("100 km/h");
+			    Beneteau.move(Beneteau);
+			    Beneteau.stop(Beneteau);
+			} 
+		}
+}
